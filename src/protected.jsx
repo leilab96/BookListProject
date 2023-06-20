07b5b258295344488daebcf3a430edd9e12b84bd
Leila <badeeb9611@gmail.com>
2023-06-20 20:36:42 +0200

    first version

---
+++ src/protected.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { getCurrentUserId } from "./auth";
+
+const Protected = ({ children }) => {
+  const userId = getCurrentUserId();
+  const location = useLocation();
+
+  if (!userId) {
+    return <Navigate to="/" state={{ from: location.pathname }} />;
+  }
+
+  return children;
+};
+
+export default Protected;
